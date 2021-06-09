990c82b97b64930e9fedfb23cdb7e61b4c612699
PimentaJoao <jppimentas@hotmail.com>
2021-06-09 02:06:08 -0300

    add two sum solution

---
+++ twoSum/twosum.go
@@ -0,0 +1,17 @@
+package main
+
+/*
+	Problem: Two Sum @ https://leetcode.com/problems/two-sum/
+*/
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return []int{}
+}
